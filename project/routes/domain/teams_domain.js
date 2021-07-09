4491dbb85991e43a17793323a9fdc9375d227aca
liadna1995 <liadna1995@gmail.com>
2021-07-09 10:12:54 +0300

    updated search teams Result : returns ID now

--- project/routes/domain/teams_domain.js
+++ project/routes/domain/teams_domain.js
@@ -3,8 +3,9 @@ const league_utils = require("../utils/league_utils");
 
 function extractRelevantTeamData(teams_info) {
     return teams_info.map((team) => {
-      const { name, logo_path} = team;
+      const { name, logo_path,id} = team;
       return {
+        "id": id,
         "team name": name,
         "logo path": logo_path,
       };
